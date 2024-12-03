ec47c4568e31749d7cd65b032f8cf33b5fc9daac
Lívia Araujo <liviaaraujo.dev@gmail.com>
2024-12-03 19:32:33 -0300

    fixes

--- src/components/SitesSalas/index.tsx
+++ src/components/SitesSalas/index.tsx
@@ -10,11 +10,11 @@ export function SiteSalas() {
 
           <h3 className="text-3xl font-bold text-[#54a532] text-center">Acopiara</h3>
           <div className='flex flex-col lg:flex-row-reverse mt-6 gap-4'>
-            <div className='w-[100%] md:w-[40%] flex flex-col justify-center items-center md:items-start text-center'>
+            <div className='w-[100%] md:w-[40%] flex flex-col justify-center items-center md:items-start text-center md:text-left'>
               <p className="text-lg">
                 A Sala do Empreendedor de Acopiara dispõe de um site exclusivo que centraliza diversos serviços voltados para apoiar empreendedores locais. Nele, os usuários podem acessar informações.
               </p>
-              <button className='bg-green-600 px-20 py-2 rounded-lg text-white font-bold mt-2'>Acessa Site</button>
+              <a className='bg-green-600 px-20 py-2 rounded-lg text-white font-bold mt-2' href="https://saladoempreendedoracopiara.vercel.app/">Acessa Site</a>
             </div>
             <div className='w-[100%] md:w-[40%]'>
               <Image
